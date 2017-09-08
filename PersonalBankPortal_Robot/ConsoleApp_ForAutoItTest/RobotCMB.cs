91c4236122ae5c9e9ad3fa765249102a57d432a6
liyan-vtcpl <li.yan@vistatechcapital.com>
2017-09-08 10:08:49 +0800

    refactor the left button click

--- PersonalBankPortal_Robot/ConsoleApp_ForAutoItTest/RobotCMB.cs
+++ PersonalBankPortal_Robot/ConsoleApp_ForAutoItTest/RobotCMB.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp_ForAutoItTest
 {
     public class RobotCMB
     {
+        private const int AutoItXSuccess = 1;
         private const string LoginFormTitle = "招商银行个人银行专业版";
         private IntPtr _mainForm;
         private Rectangle _mainFormPosition;
@@ -65,12 +66,12 @@ namespace ConsoleApp_ForAutoItTest
                 if (processExists != 0)
                 {
                     int processClose = AutoItX.ProcessClose("PersonalBankPortal.exe");
-                    if (processClose == 1)
+                    if (processClose == AutoItXSuccess)
                     {
                         Console.WriteLine("Kill old process done");
                     }
                 }
-                if (AutoItX.WinExists(LoginFormTitle) != 1)
+                if (AutoItX.WinExists(LoginFormTitle) != AutoItXSuccess)
                 {
                     AutoItX.Run("D:\\MIDAS\\CMB\\Locale.Emulator.2.3.1.1\\LEProc.exe -run \"C:\\Windows\\SysWOW64\\PersonalBankPortal.exe\"", "");
                     AutoItX.WinWait(LoginFormTitle, "", 5);
@@ -99,7 +100,7 @@ namespace ConsoleApp_ForAutoItTest
                 Stopwatch sw = new Stopwatch();
                 sw.Start();
                 int errorHappen1 = AutoItX.WinWaitActive("[TITLE:错误;CLASS:TErrorWithHelpForm]", "", 5); //token key not plugin
-                if (errorHappen1 == 1)
+                if (errorHappen1 == AutoItXSuccess)
                 {
                     AutoItX.WinClose("[TITLE:错误;CLASS:TErrorWithHelpForm]");
                     sw.Stop();
@@ -107,7 +108,7 @@ namespace ConsoleApp_ForAutoItTest
                     return RobotResult.Build(context, RobotStatus.ERROR, "Login Failed1, Error<Authentication Key Missing>");
                 }
                 int errorHappen2 = AutoItX.WinWaitActive("[CLASS:TPbBaseMsgForm]", "", 10); //login password validate
-                if (errorHappen2 == 1)
+                if (errorHappen2 == AutoItXSuccess)
                 {
                     string errorText = AutoItX.WinGetText("[CLASS:TPbBaseMsgForm]");
                     AutoItX.WinClose("[CLASS:TPbBaseMsgForm]");
@@ -116,7 +117,7 @@ namespace ConsoleApp_ForAutoItTest
                     return RobotResult.Build(context, RobotStatus.ERROR, $"Login Failed2, Error<{errorText.Trim()}>");
                 }
                 int errorHappen3 = AutoItX.WinWaitActive("[TITLE:招商银行个人银行专业版;CLASS:TMainFrm]", "功能", 60); //main portal window
-                if (errorHappen3 == 1)
+                if (errorHappen3 == AutoItXSuccess)
                 {
                     _mainForm = AutoItX.WinGetHandle("[TITLE:招商银行个人银行专业版;CLASS:TMainFrm]", "功能");
                     _mainFormPosition = AutoItX.WinGetPos(_mainForm);
@@ -125,7 +126,7 @@ namespace ConsoleApp_ForAutoItTest
                     return RobotResult.Build(context, RobotStatus.SUCCESS, "Login Success, Awesome!");
                 }
                 int errorHappen4 = AutoItX.WinWaitActive("[TITLE:错误;CLASS:TErrorWithHelpForm]", "", 5); //main portal window
-                if (errorHappen4 == 1)
+                if (errorHappen4 == AutoItXSuccess)
                 {
                     AutoItX.WinClose("[TITLE:错误;CLASS:TErrorWithHelpForm]");
                     sw.Stop();
@@ -146,9 +147,11 @@ namespace ConsoleApp_ForAutoItTest
         {
             try
             {
-                Console.WriteLine("Do Transfer Out1");
-                Thread.Sleep(TimeSpan.FromSeconds(15));
-                Console.WriteLine("Do Transfer Out2");
+                for (int i = 0; i < 7; i++)
+                {
+                    Console.WriteLine("Do Transfer Out" + i);
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
                 return RobotResult.Build(context, RobotStatus.SUCCESS, "");
             }
             catch (Exception e)
@@ -161,56 +164,28 @@ namespace ConsoleApp_ForAutoItTest
         {
             try
             {
-                int btnLogOutPossitionX = _mainFormPosition.X + _mainFormPosition.Width - 140;
-                int btnLogOutPossitionY = _mainFormPosition.Y + 17;
-                Console.WriteLine("Click Log Out");
-                AutoItX.MouseMove(_mainFormPosition.X, _mainFormPosition.Y);
-                Thread.Sleep(TimeSpan.FromSeconds(2));
-                AutoItX.MouseMove(btnLogOutPossitionX, btnLogOutPossitionY);
-                Thread.Sleep(TimeSpan.FromSeconds(2));
-                AutoItX.MouseClick("LEFT", btnLogOutPossitionX, btnLogOutPossitionY);
+                ClickButton(_mainFormPosition.X, _mainFormPosition.Y, _mainFormPosition.Width - 140, 17);
 
                 int warningHappen1 = AutoItX.WinWaitActive("[CLASS:TAppExitForm]", "", 5);
-                if (warningHappen1 == 1)
+                if (warningHappen1 == AutoItXSuccess)
                 {
                     IntPtr warningPopWin1 = AutoItX.WinGetHandle("[CLASS:TAppExitForm]");
                     Rectangle warningPopWinPossition1 = AutoItX.WinGetPos(warningPopWin1);
-                    int btnYesPossitionX1 = warningPopWinPossition1.X + 110;
-                    int btnYesPossitionY1 = warningPopWinPossition1.Y + 190;
-                    Console.WriteLine("Click Yes1 Button");
-                    AutoItX.MouseMove(warningPopWinPossition1.X, warningPopWinPossition1.Y);
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
-                    AutoItX.MouseMove(btnYesPossitionX1, btnYesPossitionY1);
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
-                    AutoItX.MouseClick("LEFT", btnYesPossitionX1, btnYesPossitionY1);
+                    ClickButton(warningPopWinPossition1.X, warningPopWinPossition1.Y, 110, 190);
                 }
                 int warningHappen2 = AutoItX.WinWaitActive("[CLASS:TPbBaseMsgForm]", "移动证书优KEY还插在电脑", 5);
-                if (warningHappen2 == 1)
+                if (warningHappen2 == AutoItXSuccess)
                 {
                     IntPtr warningPopWin2 = AutoItX.WinGetHandle("[CLASS:TPbBaseMsgForm]", "移动证书优KEY还插在电脑");
                     Rectangle warningPopWinPossition2 = AutoItX.WinGetPos(warningPopWin2);
-                    int btnYesPossitionX2 = warningPopWinPossition2.X + 250;
-                    int btnYesPossitionY2 = warningPopWinPossition2.Y + 170;
-                    Console.WriteLine("Click Yes2 Button");
-                    AutoItX.MouseMove(warningPopWinPossition2.X, warningPopWinPossition2.Y);
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
-                    AutoItX.MouseMove(btnYesPossitionX2, btnYesPossitionY2);
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
-                    AutoItX.MouseClick("LEFT", btnYesPossitionX2, btnYesPossitionY2);
+                    ClickButton(warningPopWinPossition2.X, warningPopWinPossition2.Y, 250, 170);
                 }
                 int warningHappen3 = AutoItX.WinWaitActive("[CLASS:TPbBaseMsgForm]", "再次确认是否要不拔掉优KEY退出专业版", 5);
-                if (warningHappen3 == 1)
+                if (warningHappen3 == AutoItXSuccess)
                 {
                     IntPtr warningPopWin3 = AutoItX.WinGetHandle("[CLASS:TPbBaseMsgForm]", "再次确认是否要不拔掉优KEY退出专业版");
                     Rectangle warningPopWinPossition3 = AutoItX.WinGetPos(warningPopWin3);
-                    int btnYesPossitionX3 = warningPopWinPossition3.X + 250;
-                    int btnYesPossitionY3 = warningPopWinPossition3.Y + 160;
-                    Console.WriteLine("Click Yes3 Button");
-                    AutoItX.MouseMove(warningPopWinPossition3.X, warningPopWinPossition3.Y);
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
-                    AutoItX.MouseMove(btnYesPossitionX3, btnYesPossitionY3);
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
-                    AutoItX.MouseClick("LEFT", btnYesPossitionX3, btnYesPossitionY3);
+                    ClickButton(warningPopWinPossition3.X, warningPopWinPossition3.Y, 260, 160);
                 }
                 Thread.Sleep(TimeSpan.FromSeconds(3));
                 return RobotResult.Build(context, RobotStatus.SUCCESS, "");
@@ -221,31 +196,39 @@ namespace ConsoleApp_ForAutoItTest
             }
         }
 
-        private static void ClickLoginButton(IntPtr loginForm, IntPtr textPass)
+        private void ClickLoginButton(IntPtr loginForm, IntPtr textPass)
         {
             Rectangle loginFormPosition = AutoItX.WinGetPos(loginForm);
             Rectangle textPassPosition = AutoItX.ControlGetPos(loginForm, textPass);
-            int btnLogInPossitionX = loginFormPosition.X + textPassPosition.X + 50;
-            int btnLogInPossitionY = loginFormPosition.Y + textPassPosition.Y + 60;
-            Console.WriteLine("Click Log In");
-            AutoItX.MouseMove(loginFormPosition.X + textPassPosition.X, loginFormPosition.Y + textPassPosition.Y);
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            AutoItX.MouseMove(btnLogInPossitionX, btnLogInPossitionY);
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            AutoItX.MouseClick("LEFT", btnLogInPossitionX, btnLogInPossitionY);
+            int startX = loginFormPosition.X + textPassPosition.X;
+            int startY = loginFormPosition.Y + textPassPosition.Y;
+            ClickButton(startX, startY, 50, 60);
         }
 
-        private static void EnterTextBox(IntPtr mainWindow, IntPtr textBox, string value)
+        private void ClickButton(int startX, int startY, int offsetX, int offsetY)
         {
-            if (AutoItX.ControlFocus(mainWindow, textBox) == 1)
+            AutoItX.MouseMove(startX, startY);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            int btnPossitionX = startX + offsetX;
+            int btnPossitionY = startY + offsetY;
+            AutoItX.MouseMove(btnPossitionX, btnPossitionY);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            AutoItX.MouseDown();
+            AutoItX.MouseUp();
+        }
+
+
+        private void EnterTextBox(IntPtr mainWindow, IntPtr textBox, string value)
+        {
+            if (AutoItX.ControlFocus(mainWindow, textBox) == AutoItXSuccess)
             {
                 AutoItX.Send(value);
             }
         }
 
-        private static void ClearTextBox(IntPtr mainWindow, IntPtr textBox)
+        private void ClearTextBox(IntPtr mainWindow, IntPtr textBox)
         {
-            if (AutoItX.ControlFocus(mainWindow, textBox) == 1)
+            if (AutoItX.ControlFocus(mainWindow, textBox) == AutoItXSuccess)
             {
                 string textBoxContent = AutoItX.ControlGetText(mainWindow, textBox);
                 while (!string.IsNullOrEmpty(textBoxContent))
