8a37e6d62dc9a7cba30c435428affaa60faa1160
swlv <lhunnel.villanueva@mnltechnology.com>
2017-09-19 12:40:12 +0800

    logout

--- PersonalBankPortal_Robot/ConsoleApp_ForAutoItTest/RobotCMB.cs
+++ PersonalBankPortal_Robot/ConsoleApp_ForAutoItTest/RobotCMB.cs
@@ -29,9 +29,9 @@ namespace ConsoleApp_ForAutoItTest
         {
             return new FundOutStep[]
             {
-                DoOpenClientApp,
-                DoLogIn,
-                DoTransfer,
+                //DoOpenClientApp,
+                //DoLogIn,
+                //DoTransfer,
                 DoLogOut
             };
         }
@@ -122,7 +122,7 @@ namespace ConsoleApp_ForAutoItTest
                 SimulateKey.SetForegroundWindow(SimulateKey.FindWindow(MainWindowClass, null));
                 SimulateKey.BringWindowToTop(SimulateKey.FindWindow(MainWindowClass, null));
 
-
+                Thread.Sleep(2000);
                 inputSimulator.Mouse.MoveMouseTo(5500, 25000); //Transfer Button
                 Thread.Sleep(GetRandomNumber(100));
                 inputSimulator.Mouse.LeftButtonClick();
@@ -232,24 +232,47 @@ namespace ConsoleApp_ForAutoItTest
             Thread.Sleep(GetRandomNumber(100));
 
 
+
+            thread = new Thread(() => Clipboard.SetText(context.WithdrawAmount));
+            thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+            thread.Start();
+            thread.Join();
+
             inputSimulator.Mouse.MoveMouseTo(14000, 37500); //Amount
             Thread.Sleep(GetRandomNumber(100));
             inputSimulator.Mouse.LeftButtonClick();
-            
-            inputSimulator.Keyboard.KeyPress((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_1);
 
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
             Thread.Sleep(GetRandomNumber(100));
+            Thread.Sleep(2000);
 
 
-            inputSimulator.Mouse.MoveMouseTo(15000, 45500); //Transfer Okay
+
+            thread = new Thread(() => Clipboard.SetText(context.BoTransactionId));
+            thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+            thread.Start();
+            thread.Join();
+
+            inputSimulator.Mouse.MoveMouseTo(14000, 43000); //Transfer Reference
             Thread.Sleep(GetRandomNumber(100));
             inputSimulator.Mouse.LeftButtonClick();
 
-            //Thread.Sleep(GetRandomNumber(1000));
-            //inputSimulator.Mouse.MoveMouseTo(31000, 36500); //Transfer Okay Confirm
-            //Thread.Sleep(GetRandomNumber(100));
-            //inputSimulator.Mouse.LeftButtonClick();
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+            Thread.Sleep(GetRandomNumber(100));
+            Thread.Sleep(2000);
 
+
+
+            Thread.Sleep(GetRandomNumber(100));
+            inputSimulator.Mouse.MoveMouseTo(15000, 45500); //Transfer Okay
+            Thread.Sleep(GetRandomNumber(100));
+            inputSimulator.Mouse.LeftButtonClick();
 
             Thread.Sleep(GetRandomNumber(2000));
 
@@ -257,14 +280,8 @@ namespace ConsoleApp_ForAutoItTest
 
         private void FillSameBankTransInfo(RobotContext context)
         {
-            //IntPtr textToAccountName = AutoItX.ControlGetHandle(mainFormWindow, "[CLASS:TCMBStyleEdit; INSTANCE:2]");
-            ////EnterTextBox(mainFormWindow, textToAccountName, context.ToAccountName);
-
-            //ClickToFocus(mainFormWindow, textToAccountName);
-            //AutoItX.ClipPut(context.ToAccountName);
             Thread.Sleep(2000);
-
-
+            
             Thread thread = new Thread(() => Clipboard.SetText(context.ToAccountName));
             thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
             thread.Start();
@@ -279,10 +296,9 @@ namespace ConsoleApp_ForAutoItTest
             inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
             inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode) VirtualKeyCode.LCONTROL);
             Thread.Sleep(GetRandomNumber(100));
-            Thread.Sleep(5000);
-
-
+            Thread.Sleep(2000);
             
+
 
             thread = new Thread(() => Clipboard.SetText(context.ToAccountNumber));
             thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
@@ -298,86 +314,110 @@ namespace ConsoleApp_ForAutoItTest
             inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
             inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
             Thread.Sleep(GetRandomNumber(100));
-            Thread.Sleep(5000);
+            Thread.Sleep(2000);
 
 
+            if (!string.IsNullOrEmpty(context.ToAccountCity))
+            {
+                //City Name COPY
+                thread = new Thread(() => Clipboard.SetText(context.ToAccountCity));
+                thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+                thread.Start();
+                thread.Join();
+
+                inputSimulator.Mouse.MoveMouseTo(14000, 31000); //City Name
+                Thread.Sleep(GetRandomNumber(100));
+                inputSimulator.Mouse.LeftButtonClick();
+                Thread.Sleep(GetRandomNumber(200));
 
 
+                inputSimulator.Mouse.MoveMouseTo(14000, 33000); //city Search
+                Thread.Sleep(GetRandomNumber(100));
+                inputSimulator.Mouse.LeftButtonClick();
+                Thread.Sleep(GetRandomNumber(200));
+
+                inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+                inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+                inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+                inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+                Thread.Sleep(GetRandomNumber(100));
+                Thread.Sleep(1000);
+
+                inputSimulator.Mouse.MoveMouseTo(14000, 35000); //city Select
+                Thread.Sleep(GetRandomNumber(100));
+                inputSimulator.Mouse.LeftButtonClick();
+                Thread.Sleep(GetRandomNumber(100));
+            }
+            
+            thread = new Thread(() => Clipboard.SetText(context.WithdrawAmount));
+            thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+            thread.Start();
+            thread.Join();
 
             inputSimulator.Mouse.MoveMouseTo(14000, 33500); //Amount
             Thread.Sleep(GetRandomNumber(100));
             inputSimulator.Mouse.LeftButtonClick();
 
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+            Thread.Sleep(GetRandomNumber(100));
+            Thread.Sleep(2000);
 
-            inputSimulator.Keyboard.KeyPress((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_1);
 
+
+            thread = new Thread(() => Clipboard.SetText(context.BoTransactionId));
+            thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+            thread.Start();
+            thread.Join();
+
+            inputSimulator.Mouse.MoveMouseTo(14000, 39000); //Transfer Reference
+            Thread.Sleep(GetRandomNumber(100));
+            inputSimulator.Mouse.LeftButtonClick();
+
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
+            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
+            Thread.Sleep(GetRandomNumber(100));
+            Thread.Sleep(2000);
 
 
 
             inputSimulator.Mouse.MoveMouseTo(15000, 43500); //Transfer Okay
             Thread.Sleep(GetRandomNumber(100));
             inputSimulator.Mouse.LeftButtonClick();
-
-            Thread.Sleep(GetRandomNumber(1000));
-            inputSimulator.Mouse.MoveMouseTo(31000, 36500); //Transfer Okay Confirm
             Thread.Sleep(GetRandomNumber(100));
-            inputSimulator.Mouse.LeftButtonClick();
 
-
-            Thread.Sleep(GetRandomNumber(2000));
-            //IntPtr textToAccountNumber = AutoItX.ControlGetHandle(mainFormWindow, "[CLASS:TCMBStyleEdit; INSTANCE:3]");
-            //EnterAccountNumber(mainFormWindow, textToAccountNumber, context.ToAccountNumber);
-
-            ////IntPtr searchComboBoxToAccountCity = AutoItX.ControlGetHandle(mainFormWindow, "[CLASS:TCMBSearchComboBox; INSTANCE:1]");
-            ////SearchAndSelectComboBox(mainFormWindow, searchComboBoxToAccountCity, context.ToAccountCity);
-            //Thread.Sleep(2000);
-            //IntPtr textTransferAmount = AutoItX.ControlGetHandle(mainFormWindow, "[CLASS:TCMBStyleEdit; INSTANCE:4]");
-            //EnterOtpBox(mainFormWindow, textTransferAmount, context.WithdrawAmount);
-
-            //IntPtr textPostscript = AutoItX.ControlGetHandle(mainFormWindow, "[CLASS:TCMBStyleComboBox; INSTANCE:1]");
-            ////EnterOtpBox(mainFormWindow, textPostscript, context.BoTransactionId);
-
-            //ClickButton(mainFormWindow, 350, 640); // click 'Next' button
-            //int warningHappen1 = AutoItX.WinWaitActive("[CLASS:TPbBaseMsgForm]", "选择的收款方地址与收款方账户所属开户地不符", 10);
-            //if (warningHappen1 == AutoItXSuccess)
-            //{
-            //    IntPtr warningPopWin1 = AutoItX.WinGetHandle("[CLASS:TPbBaseMsgForm]", "选择的收款方地址与收款方账户所属开户地不符");
-            //    ClickButton(warningPopWin1, 300, 150);
-            //}
+            for (int j = 0; j < 5; j++)
+            {
+                var popup = SimulateKey.SetForegroundWindow(SimulateKey.FindWindow("TPbBaseMsgForm", null));
+                if (popup)
+                {
+                    Thread.Sleep(GetRandomNumber(1000));
+                    inputSimulator.Mouse.MoveMouseTo(31000, 36500); //Transfer Okay Confirm
+                    Thread.Sleep(GetRandomNumber(100));
+                    inputSimulator.Mouse.LeftButtonClick();
+                    break;
+                }
+                Thread.Sleep(1000);
+            }
+            
+            Thread.Sleep(GetRandomNumber(1000));
+            
         }
 
         private void FillTransferPassword(RobotContext context)
         {
-            //ClickButton(mainFormWindow, 550, 410); // click '获取短信验证码' button
-
-            //int warningHappen1 = AutoItX.WinWaitActive("[CLASS:TPbBaseMsgForm]", "选择通过短信方式获取验证码", 5);
-            //if (warningHappen1 == AutoItXSuccess)
-            //{
-            //    IntPtr warningPopWin1 = AutoItX.WinGetHandle("[CLASS:TPbBaseMsgForm]", "选择通过短信方式获取验证码");
-            //    ClickButton(warningPopWin1, 250, 170);
-            //}
-            //int warningHappen2 = AutoItX.WinWaitActive("[CLASS:TPbBaseMsgForm]", "通过短信方式获取验证码的请求提交成功", 10);
-            //if (warningHappen2 == AutoItXSuccess)
-            //{
-            //    IntPtr warningPopWin2 = AutoItX.WinGetHandle("[CLASS:TPbBaseMsgForm]", "通过短信方式获取验证码的请求提交成功");
-            //    ClickButton(warningPopWin2, 300, 160);
-            //}
-
-            // wait to get OTP
-            context.Otp = "452541";
 
             var y =  AutoItX.ControlGetHandle(SimulateKey.FindWindow(MainWindowClass, null), "[CLASS:TCMBEdit; INSTANCE:1]");
-
-
-            //
-            //0x00d904fc
             inputSimulator.Mouse.MoveMouseTo(18000, 34500); //Transferpassword
             Thread.Sleep(GetRandomNumber(100));
             inputSimulator.Mouse.LeftButtonClick();
 
-
             Thread.Sleep(GetRandomNumber(10));
-            SimulateKey.SendText(y,context.Otp);
+            SimulateKey.SendText(y,context.TokenWithdrawPin);
 
             Thread.Sleep(GetRandomNumber(10));
 
@@ -399,16 +439,52 @@ namespace ConsoleApp_ForAutoItTest
             inputSimulator.Mouse.LeftButtonClick();
 
 
-            //IntPtr textOtpBox = AutoItX.ControlGetHandle(mainFormWindow, "[CLASS:TCMBEdit; INSTANCE:1]");
-            //EnterOtpBox(mainFormWindow, textOtpBox, context.Otp);
-            //ClickButton(mainFormWindow, 420, 530); // click 'Next' button
-            Thread.Sleep(TimeSpan.FromSeconds(5));
+            for (int j = 0; j < 10; j++)
+            {
+                var popup = SimulateKey.SetForegroundWindow(SimulateKey.FindWindow("TTransferSuccessFrm", null));
+                if (popup)
+                {
+                    //Thread.Sleep(GetRandomNumber(1000));
+                    //inputSimulator.Mouse.MoveMouseTo(31000, 36500); //Transfer Okay Confirm
+                    //Thread.Sleep(GetRandomNumber(100));
+                    //inputSimulator.Mouse.LeftButtonClick();
+
+                    break;
+                }
+                Thread.Sleep(1000);
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
         }
 
         private RobotResult DoLogOut(RobotContext context)
         {
             try
             {
+                Thread.Sleep(2000);
+                SimulateKey.SetForegroundWindow(SimulateKey.FindWindow(MainWindowClass, null));
+                SimulateKey.BringWindowToTop(SimulateKey.FindWindow(MainWindowClass, null));
+
+                inputSimulator.Mouse.MoveMouseTo(57000, 6000); //Logout Button
+                Thread.Sleep(GetRandomNumber(100));
+                inputSimulator.Mouse.LeftButtonClick();
+                Thread.Sleep(1000);
+
+                inputSimulator.Mouse.MoveMouseTo(30000, 37000); //Logout Button
+                Thread.Sleep(GetRandomNumber(100));
+                inputSimulator.Mouse.LeftButtonClick();
+                Thread.Sleep(1000);
+
+                inputSimulator.Mouse.MoveMouseTo(30000, 37000); //Logout Button
+                Thread.Sleep(GetRandomNumber(100));
+                inputSimulator.Mouse.LeftButtonClick();
+                Thread.Sleep(1000);
+
+                inputSimulator.Mouse.MoveMouseTo(30000, 37000); //Logout Button
+                Thread.Sleep(GetRandomNumber(100));
+                inputSimulator.Mouse.LeftButtonClick();
+                Thread.Sleep(1000);
+
                 //IntPtr mainFormWindow = GetMainFormWindow();
                 //AutoItX.WinActivate(mainFormWindow);
 
@@ -433,6 +509,9 @@ namespace ConsoleApp_ForAutoItTest
                 //    IntPtr warningPopWin3 = AutoItX.WinGetHandle("[CLASS:TPbBaseMsgForm]", "再次确认是否要不拔掉优KEY退出专业版");
                 //    ClickButton(warningPopWin3, 260, 160);
                 //}
+
+
+
                 Thread.Sleep(TimeSpan.FromSeconds(2));
                 return RobotResult.Build(context, RobotStatus.SUCCESS, "");
             }
@@ -444,24 +523,6 @@ namespace ConsoleApp_ForAutoItTest
         #endregion
 
         #region Helper Methods
-
-      
-        private void EnterAccountNumber(IntPtr mainWindow, IntPtr textBox, string value)
-        {
-
-            //ClickToFocus(mainWindow, textBox);
-            //AutoItX.ClipPut(value);
-
-            //var inputSimulator = new InputSimulator();
-            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
-            inputSimulator.Keyboard.KeyDown((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
-            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.VK_V);
-            inputSimulator.Keyboard.KeyUp((WindowsInput.Native.VirtualKeyCode)VirtualKeyCode.LCONTROL);
-            Thread.Sleep(GetRandomNumber(100));
-
-
-            
-        }
 
         private void EnterPassword()
         {
--- PersonalBankPortal_Robot/ConsoleApp_ForAutoItTest/RobotCMBTest.cs
+++ PersonalBankPortal_Robot/ConsoleApp_ForAutoItTest/RobotCMBTest.cs
@@ -18,17 +18,18 @@ namespace ConsoleApp_ForAutoItTest
             string[] acctNames = new[] { "吕文斌", "阿拉木斯", "" };
             string[] acctNums = new[] { "6214837694277025", "6222620910034272595", "" };
             int loopTimes = 2;
-            for (int i = 1; i < loopTimes; i++)
+            for (int i = 0; i < loopTimes; i++)
             {
                 RobotContext context = new RobotContext();
                 context.MidasTransactionId = "100100" + i;
                 context.LoginPassword = "aa254172";
+                context.TokenWithdrawPin = "452541";
                 context.ToBankName = bankNames[i];
                 context.ToAccountCity = "苏州";
                 context.ToAccountName = acctNames[i];
                 context.ToAccountNumber = acctNums[i];
                 context.WithdrawAmount = "1.00";
-                context.BoTransactionId = "123-4567-8901";
+                context.BoTransactionId = "123-4567-890" + i;
                 RobotCMB robot = new RobotCMB();
                 robot.Transfer(context);
                 Thread.Sleep(TimeSpan.FromSeconds(3));
